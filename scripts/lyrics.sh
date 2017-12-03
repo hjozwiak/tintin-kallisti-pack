5d491010131710a0c58f852fe847b5d6bdac899b
Storm Dragon <stormdragon2976@gmail.com>
2017-12-03 16:59:17 -0500

    new lyrics cript added.

--- scripts/lyrics.sh
+++ scripts/lyrics.sh
@@ -1,64 +1,50 @@
 #!/bin/bash
 
-#This script created for tintin-alteraeon by Anonymous
-#released under the terms of the WTFPL: http://wtfpl.net/
-
-if [ $# -ne 2 ] ; then
-echo "#showme {Usage, \"channel\" \"track name\"}"
-exit 0
-fi
-
-#get the lyric text into a variable
-trackName="${2//Ã/o}"
-trackName="$(echo "$trackName" | sed -r -e "s/((\w|\d| |\.|!|\?|')+ - (\w|\d| |\.|!|\?|')+)( +\[| +\(| +- ).*/\1/")"
-echo "#showme {Searching for $trackName...}"
-# Random agent string
-agent="$(shuf -n 1 -e \
-    "Mozilla/5.0 (Android; Mobile; rv:40.0) Gecko/40.0 Firefox/40.0"\
-    "Mozilla/5.0 (Android; Tablet; rv:40.0) Gecko/40.0 Firefox/40.0"\
-    "Mozilla/5.0 (Linux; Android 4.4; Nexus 5 Build/_BuildID_) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/30.0.0.0 Mobile Safari/537.36")"
-# Get our curl command
-unset curl
-if command -v torify &> /dev/null ; then
-curl="torify "
-fi
-curl="${curl}$(command -v curl) "
-curl="${curl}--connect-timeout 5 "
-#Lyricsmania
-trackName="$(echo "$trackName" | sed -e "s/ /_/g" -e "s/([[:print:]]*)//g" -e "s/['\/\.]//g" -e 's/&/and/g')"
-artist="$(echo "${trackName,,}" | cut -d "-" -f 1 | sed -e 's/_$//' -e 's/^the_\(.*\)/\1_the/')"
-song="$(echo "${trackName,,}" | cut -d "-" -f 2 | sed 's/^_//')"
-lyricsUrl="http://www.lyricsmania.com/${song}_lyrics_${artist}.html"
-lyricText="$($curl -s "$lyricsUrl" | grep -A 100 '<strong>Lyrics to ' | grep -B 100 '</div> <!-- lyrics-body -->' | tail -n +2 | head -n -2 | sed -e 's/<[^>]*>//g' -e 's/<br \/>/\n/g' -e 's/<\/div>.*//')"
-#get the number of lines in the text 
-lyricMaxLines=$(echo "$lyricText" | wc -l)
-#keep track of 10 line chunks of text for your starting point.
-x=10
-lyricStartLine=$x
-while [[ $x -le $(($lyricMaxLines - 10)) ]] ; do
-x=$((x + 10))
-lyricStartLine="$lyricStartLine $x"
+# Dependancy check 
+for i in clyrics fortune ; do
+if ! command -v $i &> /dev/null ; then
+    echo "{#echo Please install $i before using this script.}"
+  exit 1
+  fi
 done
-#pass the starting options through shuf so we can get our starting line
-lyricStartLine=$(shuf -n 1 -e $lyricStartLine)
-#we only need to do the head, tail manipulation if there's more than one line.
-if [ $lyricMaxLines -gt 10 ] ; then
-    lyricText="$(echo "$lyricText" | tail -n $lyricStartLine | head -n 10)"
+
+# Path where lyrics are stored
+# Most likely ~/.local/share/fortunes
+lyricsPath="${XDG_DATA_HOME:-$HOME/.local/share}/fortune"
+mkdir -p "$lyricsPath"
+
+# Get the song and artist info
+artist="$(echo "$2" | cut -d '-' -f1 | tr -s '[:space:]')"
+artist="${artist%[[:space:]]}"
+song="$(echo "$2" | cut -d '-' -f2 | cut -d '(' -f1 | tr -s '[:space:]')"
+song="${song%[[:space:]]}"
+lyricsFile="${song// /-}.${artist// /-}"
+
+# Create a fortune file containing the lyrics
+if ! [[ -f "$lyricsPath/$lyricsFile" ]]; then
+  clyrics "$song : $artist" > "$lyricsPath/$lyricsFile"
+  sed -i 's/^$/%/g' "$lyricsPath/$lyricsFile"
+   strfile "$lyricsPath/$lyricsFile" &> /dev/null
+  # Remove the files if they don't contain anything
+  [[ -s "$lyricsPath/$lyricsFile" ]] || rm "$lyricsPath/$lyricsFile" "$lyricsPath/$lyricsFile.dat"
 fi
-#Make sure all white space is not an enter or vertical space of any kind.
-lyricText="$(echo "$lyricText" | tr "[:space:]" " " | tr -s "[:space:]")"
-#sometimes lyrics are just one long line with everything. If this happens, cut it down so it's not too long for the 512 character channel limit.
-if [ ${#lyricText} -gt 412 ] ; then
-    lyricText="${lyricText:0:409}... "
-    lyricText="$(echo "$lyricText" | rev | cut -d " " -f 1- | rev)"
+
+# Sometimes the file is created, but it's one long string
+# Try to protect against that since the one string will most likely be too long for the 400 character limit
+if [[ -s "$lyricsPath/$lyricsFile" ]]; then
+  if ! grep '%' "$lyricsPath/$lyricsFile" &> /dev/null ; then 
+    rm "$lyricsPath/$lyricsFile.dat"
+    tmp="$(fold -s "$lyricsPath/$lyricsFile" | sed '0~4 s/$/\n%/g')"
+    echo "$tmp" > "$lyricsPath/$lyricsFile"
+     strfile "$lyricsPath/$lyricsFile" &> /dev/null
+  fi
 fi
-#remove simi-colons
-lyricText="$(echo "$lyricText" | tr ';' ':' | sed 's/try { _402_Show(): } catch(e) {}//')"
-#Display the lyric text
-if [ ${#lyricText} -gt 15 ] ; then
-echo "$1 ${lyricText}-- it's \$msg"
-echo "#variable {lyricsUrl} {$lyricsUrl}"
-exit 0
+
+# Display the lyrics or an error if nothing is found
+if [[ -e "$lyricsPath/$lyricsFile" ]]; then
+  echo "$1 $(fortune -n400 -s -- "$lyricsPath/$lyricsFile" | tr '[:space:]' ' ') -- it's $2."
+else
+  echo "#echo {No lyrics found for $song by $artist.}"
 fi
-echo "#showme {no lyrics found for $2.}"
+
 exit 0
